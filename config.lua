3cc68f9accc7a23b822ab4153e77c763f19cb1ec
miguelortis <100388029+miguelortis@users.noreply.github.com>
2026-09-19 11:23:14 -0400

    Add configurable bind blacklist

--- config.lua
+++ config.lua
@@ -1,5 +1,22 @@
 Config = {
-    Command = 'keybinds',
-    StoragePrefix = 'pv_keybinder:',
-    Debug = false
+    Command = 'bind',
+    StoragePrefix = 'pv_keybinder:bind:',
+    Debug = false,
+
+    Blacklist = {
+        W = true, A = true, S = true, D = true,
+        UP = true, DOWN = true, LEFT = true, RIGHT = true,
+        SPACE = true,
+        LCTRL = true, RCTRL = true,
+        LSHIFT = true, RSHIFT = true,
+        LALT = true, RALT = true,
+        TAB = true, ESC = true, CAPSLOCK = true,
+
+        F1 = true, F2 = true, F3 = true, F4 = true,
+        F5 = true, F6 = true, F7 = true, F8 = true,
+        F10 = true, F11 = true, F12 = true,
+        TILDE = true
+    },
+
+    MaxBinds = 50
 }
