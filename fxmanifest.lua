9ca5399d523401d0fa5ee5b28480e63f2f9807ba
miguelortis <100388029+miguelortis@users.noreply.github.com>
2026-09-19 12:26:41 -0400

    Enable pv_keybinder NUI menu

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,9 +3,17 @@ game 'gta5'
 
 author 'PV Development'
 description 'Persistent, framework-agnostic key bindings for FiveM'
-version '1.1.2'
+version '1.2.0'
 
 lua54 'yes'
 
 shared_script 'config.lua'
 client_script 'client/main.lua'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/app.js'
+}
