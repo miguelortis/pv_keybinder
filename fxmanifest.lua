467ad77016c4bc6e5bc3e40d8d4f400634c92d9d
miguelortis <100388029+miguelortis@users.noreply.github.com>
2026-09-19 11:30:30 -0400

    Fix config loading order

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,9 +3,12 @@ game 'gta5'
 
 author 'PV Development'
 description 'Persistent, framework-agnostic key bindings for FiveM'
-version '1.0.0'
+version '1.0.1'
 
 lua54 'yes'
+
+-- Config must be loaded before client/main.lua.
+shared_script 'config.lua'
 
 client_script 'client/main.lua'
 server_script 'server/main.lua'
