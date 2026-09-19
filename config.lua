a99dfe21f17d106628e5e1ab67cf065e7695a2f3
miguelortis <100388029+miguelortis@users.noreply.github.com>
2026-09-19 11:28:24 -0400

    Refine keyboard blacklist

--- config.lua
+++ config.lua
@@ -3,20 +3,48 @@ Config = {
     StoragePrefix = 'pv_keybinder:bind:',
     Debug = false,
 
+    MaxBinds = 50,
+
+    -- FiveM keyboard inputs that players are not allowed to use.
     Blacklist = {
-        W = true, A = true, S = true, D = true,
-        UP = true, DOWN = true, LEFT = true, RIGHT = true,
+        W = true,
+        A = true,
+        S = true,
+        D = true,
+
+        UP = true,
+        DOWN = true,
+        LEFT = true,
+        RIGHT = true,
+
         SPACE = true,
-        LCTRL = true, RCTRL = true,
-        LSHIFT = true, RSHIFT = true,
-        LALT = true, RALT = true,
-        TAB = true, ESC = true, CAPSLOCK = true,
 
-        F1 = true, F2 = true, F3 = true, F4 = true,
-        F5 = true, F6 = true, F7 = true, F8 = true,
-        F10 = true, F11 = true, F12 = true,
-        TILDE = true
-    },
+        LSHIFT = true,
+        RSHIFT = true,
+        LCONTROL = true,
+        RCONTROL = true,
+        LMENU = true,
+        RMENU = true,
 
-    MaxBinds = 50
+        TAB = true,
+        ESCAPE = true,
+        CAPSLOCK = true,
+
+        -- Reserve common FiveM / server keys.
+        F1 = true,
+        F2 = true,
+        F3 = true,
+        F4 = true,
+        F5 = true,
+        F6 = true,
+        F7 = true,
+        F8 = true,
+        F10 = true,
+        F11 = true,
+        F12 = true,
+
+        LWIN = true,
+        RWIN = true,
+        APPS = true
+    }
 }
