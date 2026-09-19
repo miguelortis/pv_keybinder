f2cf4e9e0cf95b2ab4f832b6c0b2fcc76d0a7bea
miguelortis <100388029+miguelortis@users.noreply.github.com>
2026-09-19 11:19:05 -0400

    Add fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'PV Development'
+description 'Persistent, framework-agnostic key bindings for FiveM'
+version '1.0.0'
+
+lua54 'yes'
+
+client_script 'client/main.lua'
+server_script 'server/main.lua'
