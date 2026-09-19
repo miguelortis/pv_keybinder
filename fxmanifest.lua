d47f0530d6173ff51247f56cd17cbea9edfa2da9
miguelortis <100388029+miguelortis@users.noreply.github.com>
2026-09-19 11:53:53 -0400

    Remove unnecessary server script and bump version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,12 +3,9 @@ game 'gta5'
 
 author 'PV Development'
 description 'Persistent, framework-agnostic key bindings for FiveM'
-version '1.0.1'
+version '1.1.0'
 
 lua54 'yes'
 
--- Config must be loaded before client/main.lua.
 shared_script 'config.lua'
-
 client_script 'client/main.lua'
-server_script 'server/main.lua'
